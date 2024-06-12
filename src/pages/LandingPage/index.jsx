2ded06f7c2a87f9b83ee318cfb800515f7d7ad77
renanabasilva <124288351+renanabasilva@users.noreply.github.com>
2024-06-13 00:44:17 +0100

    add content, style and key function

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import style from "./LandingPage.module.css"
+
 function LandingPage() {
+
+  // const navigate = useNavigate();
+  // const functionKeyRegex = /^F(1[0-2]?|[2-9])$/;
+
+  // useEffect(() => {
+  //   const handleKeyPress = (event) => {
+  //     if(!functionKeyRegex.test(event.key)){
+  //       navigate('/dashboard');
+  //     }
+  //   };
+
+  //   window.addEventListener('keydown', handleKeyPress);
+
+  //   return () => {
+  //     window.removeEventListener('keydown', handleKeyPress);
+  //   };
+  // });
+
   return (
     <>
-      <div>
-        <h1>MEMESTEAK</h1>
-        <button>PRESS ANY KEY TO CONTINUE</button>
+      <div className={style.mainContainer}>
+        <h1 className={style.title}>MEMESTEAK</h1>
+        <NavLink to="/dashboard" className={style.button}>PRESS ANY KEY TO CONTINUE</NavLink>
+        <div className={style.imgContainer}></div>
       </div>
     </>
   );
