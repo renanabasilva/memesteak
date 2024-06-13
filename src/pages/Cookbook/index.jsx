608a2513f64de9c5b1c1f7ff743e6b137b00a3e2
renanabasilva <124288351+renanabasilva@users.noreply.github.com>
2024-06-13 17:53:23 +0100

    feat(cookbook): add content

--- src/pages/Cookbook/index.jsx
+++ src/pages/Cookbook/index.jsx
@@ -1,8 +1,14 @@
+import Header from "../../components/Header";
+
 function Cookbook() {
   return (
     <main>
+      <Header />
       <section>
         <h1>COOKBOOK</h1>
+        <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur itaque deleniti nemo dolorem neque delectus. Ducimus nostrum officia repellat corporis magnam, voluptates laborum eum, dolor quaerat est explicabo, inventore quasi?</p>
+        </div>
       </section>
     </main>
   );
