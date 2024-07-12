a2cb13d0ae67216c91dfca71bab1964f86458aa0
Renan Aba <124288351+renanabasilva@users.noreply.github.com>
2024-07-12 13:58:25 +0100

    feat: small fixes

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { isMobile } from "react-device-detect";
+import { isMobile, isTablet } from "react-device-detect";
 import styles from "./LandingPage.module.css";
 import NavigationLink from "../../components/NavigationLink";
 import SkewedLogo from "../../assets/memesteak_logo_skewed.gif";
@@ -9,6 +9,7 @@ import ChefGif from "../../assets/chef.gif";
 function LandingPage() {
   const navigate = useNavigate();
   const functionKeyRegex = /^F(1[0-2]?|[2-9])$/;
+  const isTouchDevice = isMobile || isTablet;
 
   useEffect(() => {
     const handleKeyPress = (event) => {
@@ -36,7 +37,7 @@ function LandingPage() {
           alt="Memesteak skewed logo"
         />
         <NavigationLink double linkType="internal" link="/dashboard">
-          {isMobile ? "TAP HERE TO CONTINUE" : "PRESS ANY KEY TO CONTINUE"}
+          {isTouchDevice ? "TAP HERE TO CONTINUE" : "PRESS ANY KEY TO CONTINUE"}
         </NavigationLink>
         <img
           className={styles.spotIllustration}
